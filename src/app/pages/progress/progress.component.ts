c33fc33763a504cff1c0fc4eb81673662deef674
Fernando Herrera <fernando.herrera85@gmail.com>
2020-06-25 10:35:24 -0400

    progress diseño hecho

--- src/app/pages/progress/progress.component.ts
+++ src/app/pages/progress/progress.component.ts
@@ -3,8 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-progress',
   templateUrl: './progress.component.html',
-  styles: [
-  ]
+  styleUrls: [ './progress.component.css' ]
 })
 export class ProgressComponent implements OnInit {
 
