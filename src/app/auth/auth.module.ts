74886ddd3cfe7e9485008d5d61b43d4052bf9d32
Fernando Herrera <fernando.herrera85@gmail.com>
2020-07-08 11:53:16 -0400

    antes del servicio de usuarios

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
@@ -18,7 +18,8 @@ import { RegisterComponent } from './register/register.component';
   imports: [
     CommonModule,
     RouterModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class AuthModule { }
--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,15 +1,80 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: [ './register.component.css' ]
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent {
 
-  constructor() { }
+  public formSubmitted = false;
 
-  ngOnInit(): void {
+  public registerForm = this.fb.group({
+    nombre: ['Fernando', Validators.required ],
+    email: ['test100@gmail.com', [ Validators.required, Validators.email ] ],
+    password: ['123456', Validators.required ],
+    password2: ['123456', Validators.required ],
+    terminos: [ true, Validators.required ],
+  }, {
+    validators: this.passwordsIguales('password', 'password2')
+  });
+
+  constructor( private fb: FormBuilder ) { }
+
+  crearUsuario() {
+    this.formSubmitted = true;
+    console.log( this.registerForm.value );
+
+    if ( this.registerForm.valid ) {
+      console.log('posteando formulario')
+    } else {
+      console.log('Formulario no es correcto...');
+    }
+
+  }
+
+  campoNoValido( campo: string ): boolean {
+    
+    if ( this.registerForm.get(campo).invalid && this.formSubmitted ) {
+      return true;
+    } else {
+      return false;
+    }
+
+  }
+
+  contrasenasNoValidas() {
+    const pass1 = this.registerForm.get('password').value;
+    const pass2 = this.registerForm.get('password2').value;
+
+    if ( (pass1 !== pass2) && this.formSubmitted ) {
+      return true;
+    } else {
+      return false;
+    }
+
+  }
+
+  aceptaTerminos() {
+    return !this.registerForm.get('terminos').value && this.formSubmitted;
+  }
+
+  passwordsIguales(pass1Name: string, pass2Name: string ) {
+
+    return ( formGroup: FormGroup ) => {
+
+      const pass1Control = formGroup.get(pass1Name);
+      const pass2Control = formGroup.get(pass2Name);
+
+      if ( pass1Control.value === pass2Control.value ) {
+        pass2Control.setErrors(null)
+      } else {
+        pass2Control.setErrors({ noEsIgual: true })
+      }
+
+
+    }
   }
 
 }
