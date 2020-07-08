71503f51f951f1776a7a55929350ecc726b244b7
Fernando Herrera <fernando.herrera85@gmail.com>
2020-07-08 09:46:47 -0400

    antes del register

---
+++ src/app/models/usuario.model.ts
@@ -0,0 +1,16 @@
+
+export class Usuario {
+
+    constructor(
+        public nombre: string,
+        public email: string,
+        public password?: string,
+        public img?: string,
+        public google?: boolean,
+        public role?: string,
+        public uid?: string,
+    ) {}
+
+
+}
+
