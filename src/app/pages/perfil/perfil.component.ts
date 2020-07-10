766a333c53fd15d0f7d7efaace57f665110dd40a
Fernando Herrera <fernando.herrera85@gmail.com>
2020-07-10 14:14:54 -0400

    Fin sección 15

--- src/app/pages/perfil/perfil.component.ts
+++ src/app/pages/perfil/perfil.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import Swal from 'sweetalert2';
+
 import { UsuarioService } from '../../services/usuario.service';
+import { FileUploadService } from '../../services/file-upload.service';
+
 import { Usuario } from '../../models/usuario.model';
 
 @Component({
@@ -13,10 +17,12 @@ export class PerfilComponent implements OnInit {
 
   public perfilForm: FormGroup;
   public usuario: Usuario;
-
+  public imagenSubir: File;
+  public imgTemp: any = null;
 
   constructor( private fb: FormBuilder,
-               private usuarioService: UsuarioService ) {
+               private usuarioService: UsuarioService,
+               private fileUploadService: FileUploadService) {
     
     this.usuario = usuarioService.usuario;
   }
@@ -36,7 +42,42 @@ export class PerfilComponent implements OnInit {
           const { nombre, email } = this.perfilForm.value;
           this.usuario.nombre = nombre;
           this.usuario.email = email;
+
+          Swal.fire('Guardado', 'Cambios fueron guardados', 'success');
+        }, (err) => {
+          Swal.fire('Error', err.error.msg, 'error');
         });
+  }
+
+
+  cambiarImagen( file: File ) {
+    this.imagenSubir = file;
+
+    if ( !file ) { 
+      return this.imgTemp = null;
+    }
+
+    const reader = new FileReader();
+    reader.readAsDataURL( file );
+
+    reader.onloadend = () => {
+      this.imgTemp = reader.result;
+    }
+
+  }
+
+  subirImagen() {
+
+    this.fileUploadService
+      .actualizarFoto( this.imagenSubir, 'usuarios', this.usuario.uid )
+      .then( img => {
+        this.usuario.img = img;
+        Swal.fire('Guardado', 'Imagen de usuario actualizada', 'success');
+      }).catch( err => {
+        console.log(err);
+        Swal.fire('Error', 'No se pudo subir la imagen', 'error');
+      })
+
   }
 
 }
---
+++ src/app/services/file-upload.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+
+const base_url = environment.base_url;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FileUploadService {
+
+  constructor() { }
+
+  async actualizarFoto(
+    archivo: File,
+    tipo: 'usuarios'|'medicos'|'hospitales',
+    id: string
+  ) {
+
+    try {
+
+      const url = `${ base_url }/upload/${ tipo }/${ id }`;
+      const formData = new FormData();
+      formData.append('imagen', archivo);
+
+      const resp = await fetch( url, {
+        method: 'PUT',
+        headers: {
+          'x-token': localStorage.getItem('token') || ''
+        },
+        body: formData
+      });
+
+      const data = await resp.json();
+
+      if ( data.ok ) {
+        return data.nombreArchivo;
+      } else {
+        console.log(data.msg);
+        return false;
+      }
+      
+    } catch (error) {
+      console.log(error);
+      return false;    
+    }
+
+  }
+
+
+
+}
