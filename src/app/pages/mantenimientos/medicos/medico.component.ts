2858d1cc4c78b96157273e741044f9ba946acbad
Fernando Herrera <fernando.herrera85@gmail.com>
2020-07-13 14:30:13 -0400

    antes de la imagen del hospital

--- src/app/pages/mantenimientos/medicos/medico.component.ts
+++ src/app/pages/mantenimientos/medicos/medico.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+import { HospitalService } from '../../../services/hospital.service';
+import { Hospital } from '../../../models/hospital.model';
 
 @Component({
   selector: 'app-medico',
@@ -8,9 +12,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MedicoComponent implements OnInit {
 
-  constructor() { }
+  public medicoForm: FormGroup;
+  public hospitales: Hospital[] = [];
+
+  constructor( private fb: FormBuilder,
+               private hospitalService: HospitalService ) { }
 
   ngOnInit(): void {
+
+    this.medicoForm = this.fb.group({
+      nombre: ['Hernando', Validators.required ],
+      hospital: ['', Validators.required ],
+    });
+
+    this.cargarHospitales();
+
+  }
+
+  cargarHospitales() {
+
+    this.hospitalService.cargarHospitales()
+      .subscribe( (hospitales: Hospital[]) => {
+        console.log(hospitales)
+        this.hospitales = hospitales;
+      })
+
+  }
+
+  guardarMedico() {
+    console.log(this.medicoForm.value)
   }
 
 }
